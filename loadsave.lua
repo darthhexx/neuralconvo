fd63770617a9ef3fdf754d5acbcf817a9b1e3cda
darthhexx <dnewman@automattic.com>
2016-10-07 10:36:55 +1000

    Add module to read and write JSON to and from file
    Used for debugging.

---
+++ loadsave.lua
@@ -0,0 +1,66 @@
+local LS = {}
+local json = require("json")
+
+function LS.saveTable(t, filename)
+    local file = io.open(filename, "w")
+    if file then
+        local contents = json.encode(t)
+        file:write( contents )
+        io.close( file )
+        return true
+    else
+        return false
+    end
+end
+ 
+function LS.loadTable(filename, location)
+    local contents = ""
+    local myTable = {}
+    local file = io.open( filename, "r" )
+    if file then
+        local contents = file:read( "*a" )
+        myTable = json.decode( contents );
+        io.close( file )
+        return myTable
+    end
+    return nil
+end
+
+function LS.print_r ( t ) 
+    local print_r_cache={}
+    local function sub_print_r(t,indent)
+        if (print_r_cache[tostring(t)]) then
+            print(indent.."*"..tostring(t))
+        else
+            print_r_cache[tostring(t)]=true
+            if (type(t)=="table") then
+                for pos,val in pairs(t) do
+                    if (type(val)=="table") then
+                        print(indent.."["..pos.."] => "..tostring(t).." {")
+                        sub_print_r(val,indent..string.rep(" ",string.len(pos)+8))
+                        print(indent..string.rep(" ",string.len(pos)+6).."}")
+                    elseif (type(val)=="string") then
+                        print(indent.."["..pos..'] => "'..val..'"')
+                    else
+                        print(indent.."["..pos.."] => "..tostring(val))
+                    end
+                end
+            else
+                print(indent..tostring(t))
+            end
+        end
+    end
+    if (type(t)=="table") then
+        print(tostring(t).." {")
+        sub_print_r(t,"  ")
+        print("}")
+    else
+        sub_print_r(t,"  ")
+    end
+    print()
+end
+
+LS.printTable = LS.print_r
+
+return LS
+
